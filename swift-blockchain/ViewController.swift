4c574bda427e9d50f01b6a369c558ce946d375c5
Josh Daiter <josh.daiter@alliants.com>
2017-08-15 16:22:52 -0400

    Initial Commit

---
+++ swift-blockchain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-blockchain
+//
+//  Created by Josh Daiter on 2017-08-15.
+//  Copyright © 2017 Josh Daiter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
